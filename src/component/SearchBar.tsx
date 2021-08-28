9e9612dae8d99d7418a586288d0b041eb6a25909
Isaqb <isaqbarbosa@gmail.com>
2021-08-28 11:06:26 -0300

    Pesquisa por album,artista e streaming(sem api)

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,13 +1,17 @@
-import React, { FormEvent, useState } from 'react';
+import React, { useState } from 'react';
+import ALBUM from '../album';
 import ARTISTS from '../artist';
+import STREAMING from '../streaming';
+import AlbumItem from './AlbumItem';
 import ArtistItem from './ArtistItem';
+import StreamingItem from './StreamingItem';
 
 function SearchBar(){
     const [artists, setArtists] = useState(ARTISTS);
-    const [search, setSearch] = useState('');
+    const [searchArtist, setSearchArtist] = useState('');
     
-    const filteredArtists = search.length === 0 ? [] : artists.filter(
-        artist => artist.name.toLowerCase().includes(search.toLowerCase())
+    const filteredArtists = searchArtist.length === 0 ? [] : artists.filter(
+        artist => artist.name.toLowerCase().includes(searchArtist.toLowerCase())
     );
 
     const handleRemoveArtist = (removeArtistKey: string) =>{
@@ -16,15 +20,81 @@ function SearchBar(){
             );
     };
 
+    const [album,setAlbum] = useState(ALBUM);
+    const [searchAlbum,setSearchAlbum] = useState('');
+
+    const filteredAlbum = searchAlbum.length === 0 ? [] : album.filter(
+        album => album.name_album.toLowerCase().includes(searchAlbum.toLowerCase())
+    );
+
+    const handleRemoveAlbum = (removeAlbKey:string) =>{
+        setAlbum( album =>
+                album.filter(a=>a.key_album !== removeAlbKey)
+            );
+    }
+
+    const [streaming, setStreaming] = useState(STREAMING);
+    const [searchStreaming,setSearchStreaming] = useState('');
+
+    const filteredStreaming = searchStreaming.length === 0 ? [] : streaming.filter(
+        streaming => streaming.name_streaming.toLowerCase().includes(searchStreaming.toLowerCase())
+    );
+
+    const handleRemoveStreaming = (removeStrKey: string) =>{
+        setStreaming( streaming =>
+                streaming.filter(str => str.key_streaming !== removeStrKey)
+            );
+    }
+
+    
+    const [op,setOp] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
+        event.preventDefault();
+        setOp(event.target.value);
+        if (op==="searchArtist"){
+            setSearchArtist(event.target.value)
+        }
+    }
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        event.preventDefault();
+        if (op==="searchArtist"){
+            setSearchArtist(event.target.value)
+        }
+        if (op==="searchStreaming"){
+            setSearchStreaming(event.target.value)
+        }
+        if (op==="searchAlbum"){
+            setSearchAlbum(event.target.value)
+        }
+        
+    }
+
     return(
         <>
-            <input type="text" value={search} placeholder='Faça sua busca 'onChange={(e) => setSearch(e.target.value)}/>
-
+            
+            <form >
+                <input type="text" placeholder='Faça sua busca ' onChange={(e)=>handleSearch(e)}/>
+                    <select defaultValue="" onChange={handleChange}>
+                        <option  value=""></option>
+                        <option  value="searchArtist">Artista</option>
+                        <option value="searchStreaming">Streaming</option>
+                        <option value="searchAlbum">Album</option>
+                    </select>
+            </form>
             <div>
                 <ul>
-                    {filteredArtists.map( f =>(
-                        <ArtistItem  key={f.key} artist={f} onRemoveArtist={handleRemoveArtist}  />
-                    ))}
+                    {
+                       op === 'searchArtist' ? (filteredArtists.map( f =>(
+                        <ArtistItem  key={f.key} artist={f} onRemoveArtist={handleRemoveArtist}  />))):
+                        (op === 'searchStreaming'?(
+                            filteredStreaming.map(s=>
+                                <StreamingItem key={s.key_streaming} streaming={s} onRemoveStreaming={handleRemoveStreaming} />)
+                        ):(op==='searchAlbum'?(filteredAlbum.map(a =>
+                            <AlbumItem key={a.key_album} album={a} onRemoveAlbum={handleRemoveAlbum} />)
+                        ):[]))
+                    }
                 </ul>
             </div>
         </>
